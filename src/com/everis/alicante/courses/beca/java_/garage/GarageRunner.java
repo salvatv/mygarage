1521a425d57db929a2a0a7b2c698873392dd028a
salva <salvatv@hotmail.es>
2017-07-13 17:22:33 +0200

    Añadido el jar

---
+++ src/com/everis/alicante/courses/beca/java_/garage/GarageRunner.java
@@ -0,0 +1,12 @@
+package com.everis.alicante.courses.beca.java_.garage;
+
+import com.everis.alicante.courses.becajava.garage.ui.GarageMenu;
+
+public class GarageRunner {
+
+	public static void main(String[] args) {
+		System.out.println(GarageMenu.getInstance().play());
+
+	}
+
+}
